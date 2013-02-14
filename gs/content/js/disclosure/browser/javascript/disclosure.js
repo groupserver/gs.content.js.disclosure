1464accba12a77bbb6210e396565517f34922ea6
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-14 12:29:05 +1100

    Changing to one var per closure scope.

--- gs/content/js/disclosure/browser/javascript/disclosure.js
+++ gs/content/js/disclosure/browser/javascript/disclosure.js
@@ -5,28 +5,26 @@ var GSDisclosureButton = function () {
     */
 
     // Private variables
-    // Arrows
-    var hiddenArrow = "\u25b6";
-    var shownArrow = "\u25bc";
-    var speed = 'fast';
-    // Elements of the disclosure widgets
-    var dw = ".disclosureWidget";
-    var db = ".disclosureButton";
-    var dsh = ".disclosureShowHide";
+    var hiddenArrow = "\u25b6", 
+        shownArrow = "\u25bc", 
+        speed = 'fast', 
+        dw = ".disclosureWidget", 
+        db = ".disclosureButton", 
+        dsh = ".disclosureShowHide";
     
     // Private methods
-    var get_show_hide = function(button) {
-        retval = jQuery(button).parents(dw).find(dsh);
+    function get_show_hide(button) {
+        var retval = jQuery(button).parents(dw).find(dsh);
         return retval;
     }
 
-    var buttonClicked = function () {
-        var button = jQuery(this);
-        var html = button.html();
-        var coreHTML = html.substring(2, html.length);
-        var arrow = html.substring(0, 1);
-        var showHide = get_show_hide(this);
-        var h = null;
+    function buttonClicked() {
+        var button = jQuery(this),
+            html = button.html(),
+            coreHTML = html.substring(2, html.length),
+            arrow = html.substring(0, 1),
+            showHide = get_show_hide(this),
+            h = null;
         
         if ( arrow == hiddenArrow ) {
             button.html(shownArrow+" "+coreHTML);
@@ -38,30 +36,26 @@ var GSDisclosureButton = function () {
         showHide.slideToggle(speed).attr('aria-hidden', h);
     }
     
-    var show = function (b) {
-        var button = jQuery(this);
-        var html = button.html();
-        var coreHTML = html.substring(2, html.length);
-        var showHide = get_show_hide(this);
+    function show(b) {
+        var button = jQuery(this), html = button.html(), 
+            coreHTML = html.substring(2, html.length), 
+            showHide = get_show_hide(this);
 
         button.html(shownArrow+" "+coreHTML);
         showHide.slideDown(speed).attr('aria-hidden', 'true');
     }
 
-    var hide = function (button) {
-        var button = jQuery(this);
-        var html = button.html();
-        var coreHTML = html.substring(2, html.length);
-        var showHide = get_show_hide(this);
+    function hide(b) {
+        var button = jQuery(this), html = button.html(), 
+            coreHTML = html.substring(2, html.length), 
+            showHide = get_show_hide(this);
         
         button.html(hiddenArrow+" "+coreHTML);
         showHide.slideUp(speed).attr('aria-hidden', 'false');
     }
     
-    var prepare = function (b) {
-        var showHide = null;
-        var visible = false;
-        var button = null
+    function prepare(b) {
+        var showHide = null, visible = false, button = null
         showHide = get_show_hide(this);
         visible = showHide.css('display') != 'none';
         button = jQuery(this);
